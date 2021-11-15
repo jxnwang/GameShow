3b25ac8be39acd7c1ea0bdd052c6fcfaab9756a0
jxnwang <wjx9610@gmail.com>
2021-11-15 11:30:37 -0600

    test commit

--- src/main/java/com/laioffer/gameshow/entity/Game.java
+++ src/main/java/com/laioffer/gameshow/entity/Game.java
@@ -1,80 +1,62 @@
 package com.laioffer.gameshow.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-// Jiushiwan
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = Game.Builder.class)
+
+
 public class Game {
     @JsonProperty("name")
-    private String name;
+    private final String name;
 
-    @JsonProperty("developer")
-    private String developer;
+    @JsonProperty("id")
+    private final String id;
 
-    @JsonProperty("release_time")
-    private String releaseTime;
+    @JsonProperty("box_art_url")
+    private final String boxArtUrl;
 
-    @JsonProperty("website")
-    private String website;
-
-    @JsonProperty("price")
-    private double price;
-
+    private Game(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.boxArtUrl = builder.boxArtUrl;
+    }
     public String getName() {
         return name;
     }
-
-    public String getDeveloper() {
-        return developer;
+    public String getId() {
+        return id;
+    }
+    public String getBoxArtUrl() {
+        return boxArtUrl;
     }
 
-    public String getReleaseTime() {
-        return releaseTime;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public Game(Builder builder) {
-        this.name = builder.name;
-        this.developer = builder.developer;
-        this.releaseTime = builder.releaseTime;
-        this.website = builder.website;
-        this.price = builder.price;
-    }
-
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Builder {
+        @JsonProperty("name")
         private String name;
-        private String developer;
-        private String releaseTime;
-        private String website;
-        private double price;
+        @JsonProperty("id")
+        private String id;
+        @JsonProperty("box_art_url")
+        private String boxArtUrl;
 
-        public Builder setName(String name) {
+        public Builder name(String name) {
             this.name = name;
             return this;
         }
 
-        public Builder setDeveloper(String developer) {
-            this.developer = developer;
+        public Builder id(String id) {
+            this.id = id;
             return this;
         }
 
-        public Builder setReleaseTime(String releaseTime) {
-            this.releaseTime = releaseTime;
-            return this;
-        }
-
-        public Builder setWebsite(String website) {
-            this.website = website;
-            return this;
-        }
-
-        public Builder setPrice(double price) {
-            this.price = price;
+        public Builder boxArtUrl(String boxArtUrl) {
+            this.boxArtUrl = boxArtUrl;
             return this;
         }
 
